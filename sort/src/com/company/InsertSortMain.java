25763a3be04c9612149c71c2a99f848ea051adfd
wgl <WGLwgl123>
2019-12-31 20:26:09 +0800

    TimeUtil

--- sort/src/com/company/InsertSortMain.java
+++ sort/src/com/company/InsertSortMain.java
@@ -1,8 +1,11 @@
 package com.company;
 
 import java.util.Arrays;
+import java.util.concurrent.TimeUnit;
 
 public class InsertSortMain {
+
+    public static volatile int a = 0;
 
     public static void main(String[] args) {
         int[] arr = {3, 2, 4, 9, 1, 5, 7, 6, 8};
@@ -10,6 +13,7 @@ public class InsertSortMain {
         if (arr == null || arr.length < 2) {
             return;
         }
+        TimeUnit.MINUTES.sleep(100);
         for (int i = 1; i < arr.length; i++) {
             int temp = arr[i];
             int index = -1;
